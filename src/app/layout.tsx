0cc0a7cac82c73dd5d48541429389ac6e06af477
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-02-01 21:52:49 +0900

    routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { tv } from "tailwind-variants"
+import { tv } from "tailwind-variants";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="flex text-xl justify-center items-center">Welcome to Pokemon dex</h1>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="flex text-xl justify-center items-center">
+        Welcome to Pokemon dex
+      </h1>
+      <Link href="/welcome">Let&apos;s Go</Link>
     </div>
   );
 }
